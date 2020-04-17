3652168eff5935b06f4c55c0d13ac8c7a227ed24
Jessica Cooper <jessicagcooper@gmail.com>
2020-04-17 12:19:15 -0500

    Partially implemented robot_sort with pseudocode.

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -9,23 +9,23 @@ class SortingRobot:
         self._light = "OFF"     # The state of the robot's light
         self._time = 0          # A time counter (stretch)
 
-    def can_move_right(self):
+    def can_move_towards_the_end(self):
         """
-        Returns True if the robot can move right or False if it's
+        Returns True if the robot can move towards_the_end or False if it's
         at the end of the list.
         """
         return self._position < len(self._list) - 1
 
-    def can_move_left(self):
+    def can_move_towards_the_start(self):
         """
-        Returns True if the robot can move left or False if it's
+        Returns True if the robot can move towards_the_start or False if it's
         at the start of the list.
         """
         return self._position > 0
 
-    def move_right(self):
+    def move_towards_the_end(self):
         """
-        If the robot can move to the right, it moves to the right and
+        If the robot can move to the towards_the_end, it moves to the towards_the_end and
         returns True. Otherwise, it stays in place and returns False.
         This will increment the time counter by 1.
         """
@@ -36,9 +36,9 @@ class SortingRobot:
         else:
             return False
 
-    def move_left(self):
+    def move_towards_the_start(self):
         """
-        If the robot can move to the left, it moves to the left and
+        If the robot can move to the towards_the_start, it moves to the towards_the_start and
         returns True. Otherwise, it stays in place and returns False.
         This will increment the time counter by 1.
         """
@@ -96,8 +96,45 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        #Goal: sort list
+        #Robot methods: move towards the end, move towards the start, pick up/swap items, compare item held with item in front of
+        #Robots light has 3 methods: set it to on(True), set it to off(False), check if light_is_on returns true or false
+        
+        #Plan
+        #set robot's light to on(indicates the sorting is in progress)
+        #the robot starts out holding none, none will be used as a placeholder as each swap occurs
+        # pick up first item (index 0 will then contain 'None')
+        # move towards end (to next index)
+        # call compare_item: 
+        # if it returns 1 (held item is greater than item in front of robot) move towards end again 
+        # if it returns -1 (held item is less than item in front robot) call swap_item
+        # keep going until move towards the end returns false,
+        # then move towards the start
+        # new conditional, if compare returns 1, swap, if compare return -1, move to start, compare, if compare returns none, swap
+        self.set_light_on()
+        self.swap_item()
+        # self.move_towards_the_end()
+        while self.light_is_on() == True:
+            if self.can_move_towards_the_end() == True:
+                self.move_towards_the_end()
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+                elif self.compare_item() == 1 and self.can_move_towards_the_end() == False:
+                    self.swap_item()
+                    self.set_light_off()
+        while self.light_is_on() == False:
+            if self.can_move_towards_the_start() == True:
+                self.move_towards_the_start()        
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_towards_the_start()    
+                elif self.compare_item() == None:
+                    self.swap_item()
+                    self.set_light_on()
+                
+        
+
 
 
 if __name__ == "__main__":
@@ -105,6 +142,8 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    # l = [15, 49, 26, 4]
 
     robot = SortingRobot(l)
 
